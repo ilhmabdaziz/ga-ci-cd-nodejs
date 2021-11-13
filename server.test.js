35620fc940c76834ab945cd7f5d1e85e145667b3
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-11-14 01:30:39 +0700

    change test to it

--- server.test.js
+++ server.test.js
@@ -3,19 +3,19 @@ const app = require("./server"); // Link to your server file
 const supertest = require("supertest");
 const request = supertest(app);
 
-test("Call the / endpoint", async (done) => {
+it("Call the / endpoint", async (done) => {
   const res = await request.get("/");
   expect(res.status).toBe(200);
   expect(res.text).toBe("This App is running properly!");
   done();
 });
-test("Call the /ping endpoint", async (done) => {
+it("Call the /ping endpoint", async (done) => {
   const res = await request.get("/ping");
   expect(res.status).toBe(200);
   expect(res.text).toBe("Pong!");
   done();
 });
-test("Call the /hello/:name", async (done) => {
+it("Call the /hello/:name", async (done) => {
   const res = await request.get("/hello/zizsy");
   expect(res.status).toBe(200);
   expect(res.body.message).toBe("Hello zizsy");
