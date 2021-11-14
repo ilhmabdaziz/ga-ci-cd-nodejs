a5f8be7a7335edf9c650fb835fdf59fcde49de93
ilhmabdaziz <ilhmabdaziz@yahoo.com>
2021-11-14 22:55:01 +0700

    edit response msg

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get("/ping", (req, res) => {
   res.send("Pong!");
 });
 app.get("/youtube", (req, res) => {
-  res.send("Hello, youtube!");
+  res.send("Hello, youtube indonesia!");
 });
 app.get("/hello/:name", (req, res) => {
   res.json({ message: greeting(req.params.name) });
